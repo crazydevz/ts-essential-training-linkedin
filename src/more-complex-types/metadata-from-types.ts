ed2662fc38330f0786e68e15e4546448b7b8854e
Talha Iqbal <talhaiqbal801@gmail.com>
2022-12-14 15:02:03 +0500

    Initial commit

---
+++ src/more-complex-types/metadata-from-types.ts
@@ -0,0 +1,67 @@
+export {};
+
+let x: Record<string, string | number> = { name: 'Wruce Bayne' };
+x.id = 1234;
+// x = 'abc';/
+////////////////////
+
+type ContactStatus = 'active' | 'inactive' | 'new';
+
+interface Address {
+	street: string;
+	province: string;
+	postalCode: string;
+}
+
+interface Contact {
+	id: number;
+	name: string;
+	status: ContactStatus;
+	address: Address;
+}
+
+interface Query<TProp> {
+	sort?: 'asc' | 'desc';
+	matches(val: TProp): boolean;
+}
+
+// type ContactQuery = Omit<
+// 	Partial<Record<keyof Contact, Query>>,
+// 	'address' | 'status'
+// >;
+
+// type ContactQuery = Partial<Pick<Record<keyof Contact, Query>, 'id' | 'name'>>;
+
+type ContactQuery = {
+	[TProp in keyof Contact]?: Query<Contact[TProp]>;
+};
+
+type RequiredContactQuery = Required<ContactQuery>;
+
+function searchContacts(contacts: Contact[], query: ContactQuery) {
+	return contacts.filter(contact => {
+		for (const property of Object.keys(contact) as (keyof Contact)[]) {
+			// get the query object for this property
+			const propertyQuery = query[property] as Query<
+				Contact[keyof Contact]
+			>;
+			// check to see if it matches
+			if (propertyQuery && propertyQuery.matches(contact[property])) {
+				return true;
+			}
+		}
+
+		return false;
+	});
+}
+
+const filteredContacts = searchContacts(
+	[
+		/* contacts */
+	],
+	{
+		id: { matches: id => id === 123 },
+		name: { matches: name => name === 'Carol Weaver' },
+		// phoneNumber: { matches: name => name === 'Carol Weaver' },
+	}
+);
